f2ce540f2c21dfc50556c896d9c7a8411343c445
alevgr <alevgr9@ya.ru>
2021-08-11 18:56:41 +0300

    add realization to :     Clear     save     get     delete     getall     size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < storage.length && storage[j] != null; j++) {
+                    storage[j] = storage[j + 1];
+                }
+            }
+        }
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return Arrays.copyOf(storage, i);
+            }
+        }
         return new Resume[0];
+
     }
 
+
     int size() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
+
