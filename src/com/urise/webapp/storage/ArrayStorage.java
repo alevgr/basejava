8f08e09461f2a800f12a882b08b94a9de0c67cd4
alevgr <alevgr9@ya.ru>
2021-09-21 11:41:50 +0300

    getIndexByUid

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,67 +2,78 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int sizeResume = 0;
+    Resume[] storage = new Resume[10_000];
+    private int countResume = 0;
+
+    private int getIndexByUid(String uuid) {
+        for (int i = 0; i < countResume; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public void clear() {
-        Arrays.fill(storage, 0, sizeResume, null);
-        sizeResume = 0;
+        Arrays.fill(storage, 0, countResume, null);
+        countResume = 0;
     }
 
     public void update(Resume r) {
-        if (get(r.getUuid()) == null) {
-            System.out.println("Не могу обновить резюме " + r.getUuid() + " - нет в списке!");
+        int index = getIndexByUid(r.getUuid());
+        if (index < 0) {
+            System.out.println("Не могу обновить резюме " + r.getUuid() + " резюме с таким UID не существует!");
             return;
         }
-        delete(r.getUuid());
-        save(r);
+        Resume buf = new Resume();
+        buf.setUuid(r.getUuid());
+        this.storage[index] = buf;
     }
 
     public void save(Resume r) {
-        if (get(r.getUuid()) == null) {
-            if (sizeResume < storage.length) {
-                storage[sizeResume] = r;
-                sizeResume++;
+        int index = getIndexByUid(r.getUuid());
+        if (index != -1) {
+            System.out.println("Не могу добавить резюме - резюме с таким UID уже существует!");
+        } else {
+            if (countResume < storage.length) {
+                storage[countResume] = r;
+                countResume++;
             } else {
                 System.out.println("Не могу добавить резюме - список переполнен!");
             }
-        } else {
-            System.out.println("Не могу добавить резюме - резюме с таким UID уже существует!");
         }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < sizeResume; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
+        int index = getIndexByUid(uuid);
+        if (index < 0) {
+            System.out.println("Не могу найти резюме " + uuid + " - нет в списке!");
+            return null;
+        } else {
+            return this.storage[index];
         }
-        return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < sizeResume; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, sizeResume - i - 1);
-                sizeResume--;
-                return;
-            }
+        int index = getIndexByUid(uuid);
+        if (index < 0) {
+            System.out.println("Не могу найти резюме " + uuid + " - нет в списке!");
+        } else {
+            System.arraycopy(storage, index + 1, storage, index, countResume - index - 1);
+            countResume--;
         }
-        System.out.println("Не могу удалить резюме " + uuid + " - его нет в списке!");
     }
 
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, sizeResume);
+        return Arrays.copyOf(storage, countResume);
     }
 
     public int size() {
-        return sizeResume;
+        return countResume;
     }
-
-
 }
 
