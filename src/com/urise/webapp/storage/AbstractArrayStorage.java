427a4ba8059cbb405110c60a92a12d0ed1e73364
alevgr <alevgr9@ya.ru>
2021-10-19 03:01:23 +0300

    HW3

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -5,7 +5,7 @@ import com.urise.webapp.model.Resume;
 import java.util.Arrays;
 
 public abstract class AbstractArrayStorage implements Storage {
-    protected static final int STORAGE_LIMIT = 100_000;
+    protected static final int STORAGE_LIMIT = 1_000;
 
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
@@ -23,6 +23,52 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            System.out.println("Не могу добавить резюме - резюме " + r.getUuid() + " уже существует!");
+        } else if (size < storage.length) {
+
+            fillElement(index, r);
+            size++;
+        } else {
+            System.out.println("Не могу добавить резюме - список переполнен!");
+        }
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            System.out.println("Не могу обновить резюме " + r.getUuid() + " резюме с таким UID не существует!");
+            return;
+        }
+        storage[index] = r;
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Не могу найти резюме " + uuid + " - нет в списке!");
+        } else {
+
+            deleteElement(index, uuid);
+            size--;
+        }
+    }
+
     protected abstract int getIndex(String uuid);
+
+    protected abstract void fillElement(int index, Resume r);
+
+    protected abstract void deleteElement(int index, String uuid);
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
 }
 
