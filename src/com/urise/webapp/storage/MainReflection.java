504eed1d30cabc579793afc7cae20a5fb0f9277f
alevgr <alevgr9@ya.ru>
2021-11-10 18:56:29 +0300

    Lesson 4.  Reflect fix

--- src/com/urise/webapp/storage/MainReflection.java
+++ src/com/urise/webapp/storage/MainReflection.java
@@ -2,19 +2,20 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
-        field.set(r, "new_uuid");
+        field.set(r, "new_uuid1");
         System.out.println(r);
 
         Method method = r.getClass().getDeclaredMethod("toString");
-        System.out.println(method);
+        System.out.println(method.invoke(r));
     }
 }
